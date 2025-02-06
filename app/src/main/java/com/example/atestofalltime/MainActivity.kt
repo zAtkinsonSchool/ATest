bbf0d9a72233115ae815064e0446f6b8e02bfe48
zAtkinsSchool <zatkins1@msudenver.edu>
2025-02-05 19:19:50 -0700

    testing by adding a comment

--- app/src/main/java/com/example/atestofalltime/MainActivity.kt
+++ app/src/main/java/com/example/atestofalltime/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    //lmao just making a new thing to commit
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
